bb5add7840badbe3bd74bbff170be49037db48dd
Jora35 <150797117+Jora35@users.noreply.github.com>
2024-05-30 10:36:51 +0300

    31

--- 31/31.js
+++ 31/31.js
@@ -1,3 +1,9 @@
 "use strict";
-let str = 'asdfgh';
-alert(str.length); // выведет 6
+let str1 = 'xxx';
+let str2 = 'yyy';
+let txt  = `aaa${str1}bbb${str2}ccc`;
+alert(txt);
+
+let str3 = `abc`;
+alert(str3); // выведет 'abc'
+
