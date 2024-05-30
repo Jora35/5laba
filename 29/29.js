5a42b975c32f3388e0a031b0775adb776efa89fa
Jora35 <150797117+Jora35@users.noreply.github.com>
2024-05-30 10:24:14 +0300

    29

---
+++ 29/29.js
@@ -0,0 +1,11 @@
+"use strict";
+let str = '!!!';
+alert(str); // выведет '!!!'
+
+let str1 = 'java';
+let str2 = 'script';
+alert(str1 + str2); // выведет 'javascript'
+
+let str3 = 'hello';
+let str4 = 'world';
+alert(str3 + str4); // выведет 'javascript'
