43da0243396b3d090903df2a5b3c62685ef12f66
Jora35 <150797117+Jora35@users.noreply.github.com>
2024-05-30 11:33:19 +0300

    38

---
+++ 37/37.js
@@ -0,0 +1,2 @@
+"use strict";
+alert('abc' * 3); // выведет NaN
---
+++ 38/38.js
@@ -0,0 +1,4 @@
+"use strict";
+alert( 1 / 0); // выведет Infinity
+alert(-1 / 0); // выведет -Infinity
+alert(10 / 0); // выведет Infinity
